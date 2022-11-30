c0e34f87fea412b2bcea0f2b0c757712b2ce3e2e
Ben Bolker <bbolker@gmail.com>
2022-11-30 13:26:13 -0500

    more pkg install

--- R/pkg_install.R
+++ R/pkg_install.R
@@ -18,9 +18,10 @@ GH_core_pkgs <- list(c("bbolker","corHMM"),    ## hacked/BMB version
 
 GH_pix_pkgs <- list(c("YuLab-SMU","ggtree"))
 
-GH_pkgs <- sapply(c(GH_core_pkgs, GH_pix_pkgs), function(x) x[2])
+GH_pkgs <- c(GH_core_pkgs, GH_pix_pkgs)
+GH_pkg_nms <- sapply(GH_pkgs, function(x) x[2])
 
-all_pkgs <- c(CRAN_pkgs, GH_pkgs)
+all_pkgs <- c(CRAN_pkgs, GH_pkg_nms)
 
 ## redundant with `tar_option_set(packages = pkgList)` ?
 load_pkgs <- function() {
@@ -38,7 +39,7 @@ install_pkgs <- function() {
                           ## skip installed packages and packages also on GH
                           c(rownames(ip),
                             base_pkgs,
-                            sapply(GH_pkgs, function(x) x[2])))
+                            GH_pkgs))
     install.packages(to_install)
     ## since install_github checks hashes, don't bother checking whether already installed
     lapply(GH_pkgs,  function(x)
