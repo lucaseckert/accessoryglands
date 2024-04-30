aaa030f8a7087d78935949590d9e5ed79169e9ca
Ben Bolker <bbolker@gmail.com>
2024-04-29 20:34:08 -0400

    hacking bayestraits code

--- bayestraits/bayestraits-model.R
+++ bayestraits/bayestraits-model.R
@@ -12,7 +12,7 @@ library(coda)
 library(bayestestR)
 ## see http://bbolker.github.io/bbmisc/bayes/examples.html
 ## for convergence diagnostics etc.
-## however, we have to make BT output look like results from
+## however, we would have to make BT output look like results from
 ##  a stan fit/write appropriate methods ...
 
 if (!interactive()) pdf("bayestraits-pix.pdf")
@@ -31,7 +31,8 @@ trees <- .compressTipLabel(trees)
 ## FIXME: can we automate this?
 ## (4*ag + 2*pc + sc will work for the non-missing cases ...)
 tar_load(ag_compdata_tb)
-data<-ag_compdata_tb$data %>%
+
+data <- ag_compdata_tb$data %>%
     mutate(state=factor(case_when(
     (ag==0 & pc==0 & sc==0) ~ 1,
     (ag==0 & pc==0 & sc==1) ~ 2,
@@ -57,27 +58,64 @@ summary(data)
 
 #### REAL MODEL ####
 
-## command vector
-command_vec<- c("1", ## MultiState
-                "2", ## MCMC
-                "ScaleTrees", ## scaling branch lengths to a mean of 0.1
-                ## we scaled sum of branch lengths to 1; since there are >1000 branches in the phylogeny,
-                ## a SUM of 1 == a mean of < 0.001
-                ## -> figure out the scaling factor and change the PriorAll accordingly ...
-                "AddTag Root Erpetoichthys_calabaricus Mugil_liza", ## adding a tag at the root
-                "Fossil Node01 Root 2", ## fossilizing the root (fix trait value at root: group spawning, no pc/ag)
-                "Res q14 q16 q17 q18 q23 q25 q27 q28 q32 q35 q36 q38 q41 q45 q46 q47 q52 q53 q54 q58 q61 q63 q64 q67 q71 q72 q74 q76 q81 q82 q83 q85 0", ## impossible rates 
-                "Res q13 q24 q57 q68", ## care gain
-                "Res q31 q42 q75 q86", ## care loss
-                "Res q12 q34 q56 q78", ## spawn gain
-                "Res q21 q43 q65 q87", ## spawn loss
-                "PriorAll lognormal 4.236 1.41", ## setting priors lognormal with mean and sd (FIXME: adjust for scaling?)
-                "Iterations 510000", ## iterations, including burn-in
-                ## default thinning = 1000
-                "Burnin 10000") ## burn-in
+rate_prior_0 <- rate_prior <- c(4.236, 1.41)
+## BT scales branch lengths to a *mean* of 0.1 by default
 
-## if you want to run it again 
-#results <- bayestraits(data, trees, command_vec)
+edge_len_tot <- sapply(trees, \(x) sum(x$edge.length))
+stopifnot(all(abs(edge_len_tot - 1) < 1e-8))
+n_edges <- sapply(trees, \(x) nrow(x$edge))  ## all 1212
+stopifnot(var(n_edges) == 0)
+n_edge <- n_edges[[1]]
+
+prior_fac <- (1/n_edge)/0.1
+## so mean branch length in our analysis is 1/1212
+## our branch lengths are much *shorter*
+## therefore our rates will be *higher* than BT's
+## therefore we should decrease the priors we give to BT by log(prior_fac)
+## (-4.79)
+rate_prior[1] <- rate_prior_0[1] + log(prior_fac)
+prior1 <- sprintf("PriorAll lognormal %f %f", rate_prior[1], rate_prior[2])
+
+##  q14 q16 q17 q18 q23 q25 q27 q28 q32 q35 q36 q38 q41 q45 q46 q47 q52 q53 q54 q58 q61 q63 q64 q67 q71 q72 q74 q76 q81 q82 q83 q85 0", ## impossible rates 
+zero_rates <- c(14, 16:18, 23, 25, 27:28, 32, 35:36, 38, 41, 45:47, 52:54,
+                58, 61, 63:64, 67, 71:72, 74, 76, 81:83, 85)
+qz <- paste("q", zero_rates, sep ="", collapse = " ")
+bt_command <- function(prior = NULL, iterations = 51e4, burnin = 1e4, seed = 101) {
+    cvec <- c("1", ## MultiState
+              "2", ## MCMC
+              "ScaleTrees", ## scaling branch lengths to a mean of 0.1
+              "AddTag Root Erpetoichthys_calabaricus Mugil_liza", ## adding a tag at the root
+              "Fossil Node01 Root 2", ## fossilizing the root (fix trait value at root: group spawning, no pc/ag)
+              paste("Res", qz, "0"),
+              "Res q13 q24 q57 q68", ## care gain
+              "Res q31 q42 q75 q86", ## care loss
+              "Res q12 q34 q56 q78", ## spawn gain
+              "Res q21 q43 q65 q87"  ## spawn loss
+              )
+    if (!is.null(prior)) cvec <- c(cvec, prior)
+    cvec <-  c(cvec,
+               sprintf("Iterations %d", iterations),
+               sprintf("Burnin %d", burnin),
+               sprintf("Seed %d", seed)
+               )
+    return(cvec)
+}
+
+command_vec <- bt_command(prior = prior1)
+
+## run in parallel???
+options(bt_path= "BayesTraitsV4.0.0-Linux")
+options(bt_bin = "BayesTraitsV4")
+
+
+## FIXME: cache this more sensibly
+## FIXME: does this work OK with BT V4?
+## FIXME: split this file
+## FIXME: run bayestraits multiple times with distinct seeds
+## (and possibly shorter chains?) so that
+##  we can calculate Gelman-Rubin diagnostics (R-hat)
+
+## results <- bayestraits(data, trees, command_vec)
 #saveRDS(results, file = "bayestraits/bt_model_demo.rds")
 
 ## reading in results
@@ -104,8 +142,6 @@ chains1 <- get_chains(results)
 lattice::xyplot(chains1, aspect = "fill", layout = c(4,3),
                 scales = list(y = list(log = 10)))
 
-## FIXME: run bayestraits multiple times with distinct seeds so that
-##  we can calculate Gelman-Rubin diagnostics (R-hat)
 
 raftery.diag(chains1)  ## suggests we have to run longer (~4000 samples == 8 x 500)
 
@@ -123,12 +159,17 @@ cfun <- function(q1, q2, q3, q4) {
     ##        (log(q3) + log(q4))/2
     ##    ) 
 }
-contrasts<-mutate(rates, gain_care_effect =  ((q37+q48)/2)/((q15+q26)/2),
-                         gain_spawn_effect = ((q26+q48)/2)/((q15+q37)/2),
-                         gain_interaction =  ((q15+q48)/2)/((q37+q26)/2),
-                         loss_care_effect =  ((q73+q84)/2)/((q51+q62)/2),
-                         loss_spawn_effect = ((q62+q84)/2)/((q51+q73)/2),
-                         loss_interaction =  ((q51+q84)/2)/((q73+q62)/2))
+
+get_contrasts <- function(rates) {
+    mutate(rates, gain_care_effect = (q37+q48)/2-(q15+q26)/2,
+           gain_spawn_effect =  (q26+q48)/2-(q15+q37)/2,
+           gain_interaction =   (q15+q48)/2-(q37+q26)/2,
+           loss_care_effect =   (q73+q84)/2-(q51+q62)/2,
+           loss_spawn_effect =  (q62+q84)/2-(q51+q73)/2,
+           loss_interaction =   (q51+q84)/2-(q73+q62)/2)
+}
+
+contrasts <- get_contrasts(rates)
 
 ## gain contrasts
 gg_gain <- select(contrasts, gain_care_effect:gain_interaction) %>% 
@@ -155,18 +196,7 @@ summary(schedule)
 #### MODEL W/ DEFAULT PRIORS ####
 
 ## command vector
-command_vec_default<- c("1", ## MultiState
-                         "2", ## MCMC
-                         "ScaleTrees", ## scaling branch lengths to a mean of 0.1
-                         "AddTag Root Erpetoichthys_calabaricus Mugil_liza", ## adding a tag at the root
-                         "Fossil Node01 Root 2", ## fossilizing the root
-                         "Res q14 q16 q17 q18 q23 q25 q27 q28 q32 q35 q36 q38 q41 q45 q46 q47 q52 q53 q54 q58 q61 q63 q64 q67 q71 q72 q74 q76 q81 q82 q83 q85 0", ## impossible rates 
-                         "Res q13 q24 q57 q68", ## care gain
-                         "Res q31 q42 q75 q86", ## care loss
-                         "Res q12 q34 q56 q78", ## spawn gain
-                         "Res q21 q43 q65 q87", ## spawn loss
-                         "Iterations 510000", ## iterations, including burn-in
-                         "Burnin 10000") ## burn-in
+command_vec_default<- bt_command(prior = NULL)
 #results_default<-bayestraits(data, trees, command_vec_default)
 #saveRDS(results_default, file = "bayestraits/bt_model_default.rds")
 
@@ -176,12 +206,7 @@ rates_default <- results_default$Log$results
 summary(rates_default)
 
 ## computing contrasts
-gain_contrasts_default<-mutate(rates_default, gain_care_effect = ((q37+q48)/2)/((q15+q26)/2),
-                  gain_spawn_effect = ((q26+q48)/2)/((q15+q37)/2),
-                  gain_interaction = ((q15+q48)/2)/((q37+q26)/2),
-                  loss_care_effect = ((q73+q84)/2)/((q51+q62)/2),
-                  loss_spawn_effect = ((q62+q84)/2)/((q51+q73)/2),
-                  loss_interaction = ((q51+q84)/2)/((q73+q62)/2))
+gain_contrasts_default <- get_contrasts(rates_default)
 
 cdef_data <- select(gain_contrasts_default, gain_care_effect:gain_interaction) %>% 
     pivot_longer(cols=gain_care_effect:gain_interaction, names_to = "contrast")
@@ -194,19 +219,7 @@ data_dataless <- mutate(data, state=12345678)
 summary(data_dataless)
 
 ## command vector
-command_vec_dataless<- c("1", ## MultiState
-                         "2", ## MCMC
-                         "ScaleTrees", ## scaling branch lengths to a mean of 0.1
-                         "AddTag Root Erpetoichthys_calabaricus Mugil_liza", ## adding a tag at the root
-                         "Fossil Node01 Root 2", ## fossilizing the root
-                         "Res q14 q16 q17 q18 q23 q25 q27 q28 q32 q35 q36 q38 q41 q45 q46 q47 q52 q53 q54 q58 q61 q63 q64 q67 q71 q72 q74 q76 q81 q82 q83 q85 0", ## impossible rates 
-                         "Res q13 q24 q57 q68", ## care gain
-                         "Res q31 q42 q75 q86", ## care loss
-                         "Res q12 q34 q56 q78", ## spawn gain
-                         "Res q21 q43 q65 q87", ## spawn loss
-                         "PriorAll lognormal 4.236 1.41", ## setting priors lognormal with mean and sd
-                         "Iterations 510000", ## iterations, including burn-in
-                         "Burnin 10000") ## burn-in
+command_vec_dataless <- 
 #results_dataless<-bayestraits(data_dataless, trees, command_vec_dataless)
 #saveRDS(results_dataless, file = "bayestraits/bt_model_dataless.rds")
 
@@ -216,12 +229,8 @@ rates_dataless<-results_dataless$Log$results
 summary(rates_dataless)
 
 ## computing contrasts
-all_contrasts_dataless<-mutate(rates_dataless, gain_care_effect = ((q37+q48)/2)/((q15+q26)/2),
-                          gain_spawn_effect = ((q26+q48)/2)/((q15+q37)/2),
-                          gain_interaction = ((q15+q48)/2)/((q37+q26)/2),
-                          loss_care_effect = ((q73+q84)/2)/((q51+q62)/2),
-                          loss_spawn_effect = ((q62+q84)/2)/((q51+q73)/2),
-                          loss_interaction = ((q51+q84)/2)/((q73+q62)/2))
+all_contrasts_dataless <- get_contrasts(rates_dataless)
+
 cdef_all_dataless <- select(all_contrasts_dataless, gain_care_effect:gain_interaction) %>% 
     pivot_longer(cols=gain_care_effect:gain_interaction, names_to = "contrast")
 
@@ -230,19 +239,7 @@ gg_gain %+% cdef_all_dataless
 #### RJ DATA-LESS MODEL W/ DEFAULT PRIORS ####
 
 ## command vector
-command_vec_rj<- c("1", ## MultiState
-                   "2", ## MCMC
-                   "ScaleTrees", ## scaling branch lengths to a mean of 0.1
-                   "AddTag Root Erpetoichthys_calabaricus Mugil_liza", ## adding a tag at the root
-                   "Fossil Node01 Root 2", ## fossilizing the root
-                   "Res q14 q16 q17 q18 q23 q25 q27 q28 q32 q35 q36 q38 q41 q45 q46 q47 q52 q53 q54 q58 q61 q63 q64 q67 q71 q72 q74 q76 q81 q82 q83 q85 0", ## impossible rates 
-                   "Res q13 q24 q57 q68", ## care gain
-                   "Res q31 q42 q75 q86", ## care loss
-                   "Res q12 q34 q56 q78", ## spawn gain
-                   "Res q21 q43 q65 q87", ## spawn loss
-                   "RevJump uniform 0 100", ## setting priors 
-                   "Iterations 510000", ## iterations, including burn-in
-                   "Burnin 10000") ## burn-in
+command_vec_rj <- get_command(prior = "RevJump uniform 0 100")
 #results_rj<-bayestraits(data_dataless, trees, command_vec_rj)
 #saveRDS(results_rj, file = "bayestraits/bt_model_rj.rds")
 
@@ -252,12 +249,7 @@ rates_rj<-results_rj$Log$results
 summary(rates_rj)
 
 ## computing contrasts
-contrasts_rj<-mutate(rates_rj, gain_care_effect = ((q37+q48)/2)/((q15+q26)/2),
-                          gain_spawn_effect = ((q26+q48)/2)/((q15+q37)/2),
-                          gain_interaction = ((q15+q48)/2)/((q37+q26)/2),
-                          loss_care_effect = ((q73+q84)/2)/((q51+q62)/2),
-                          loss_spawn_effect = ((q62+q84)/2)/((q51+q73)/2),
-                          loss_interaction = ((q51+q84)/2)/((q73+q62)/2))
+contrasts_rj <- get_contrasts(rates_rj)
 cdef_rj <- select(contrasts_rj, gain_care_effect:gain_interaction) %>% 
     pivot_longer(cols=gain_care_effect:gain_interaction, names_to = "contrast")
 
